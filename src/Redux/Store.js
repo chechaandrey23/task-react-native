dca96380ec4539f216d1bc8637daff9e999f62ad
Andrey Checha <chechaandrey23@gmail.com>
2023-08-13 20:09:14 +0300

    first commit

---
+++ src/Redux/Store.js
@@ -0,0 +1,34 @@
+import { configureStore , combineReducers } from "@reduxjs/toolkit";
+import token from "./AuthSlice";
+import savedItem from "./SavedSlice";
+import appSetting from "./SettingSlice";
+import postProject from "./PostProjectSlice";
+import postTask from "./PostTaskSlice";
+//import storage from 'redux-persist/lib/storage'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { persistReducer } from 'redux-persist';
+import { getDefaultMiddleware } from '@reduxjs/toolkit';
+
+const rootReducer = combineReducers({
+        value: token,
+        saved: savedItem,
+        setting: appSetting,
+        postedProject:postProject,
+        postedTask:postTask
+});
+const persistConfig = {
+    //key: 'root',
+    key: 'settings',
+    storage : AsyncStorage
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+})
+
+export default store
